4075d39f42b9c8813807d18e4a28778d74b9918d
MiguelAtila <cdmx2975@amerike.edu.mx>
2025-04-16 19:53:16 -0600

    avance

--- WebAiram/script.js
+++ WebAiram/script.js
@@ -1,4 +1,13 @@
+// Carrusel misión / visión
+let index = 0;
+const carrusel = document.getElementById('carrusel');
+setInterval(() => {
+  index = (index + 1) % 2;
+  carrusel.style.transform = `translateX(-${index * 100}%)`;
+}, 5000);
+
+// Formulario
 document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Formulario enviado correctamente. ¡Gracias por contactarnos!');
+  e.preventDefault();
+  alert('Formulario enviado correctamente. ¡Gracias por contactarnos!');
 });
